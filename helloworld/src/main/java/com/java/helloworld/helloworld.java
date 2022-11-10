51237e29244e6fabd6e1daa0d1ce6c02439ee12a
Aswin1118 <aswinvj390@gmail.com>
2022-11-10 22:41:36 +0530

    Main

--- helloworld/src/main/java/com/java/helloworld/helloworld.java
+++ helloworld/src/main/java/com/java/helloworld/helloworld.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class helloworld {
+public class helloworld
+{
 
     @RequestMapping("/")
-    public String hello(){
+    public String hello()
+    {
         return "Hello World";
     }
 }
